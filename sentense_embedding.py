61f684c1f50ceef698e007abe921f5f07cde314b
qilei123 <qileimail123@gmail.com>
2020-05-05 23:45:47 -0400

    upload all

--- sentense_embedding.py
+++ sentense_embedding.py
@@ -19,18 +19,30 @@ def get_all_sentences(file_name):
     line = file_header.readline()
     sentence_list = []
     while line:
+        if line =="\n":
+            continue
         line = line.replace("\n","")
         sentence_list.append(line)
         line = file_header.readline()
     return sentence_list
 
-embedding_sentences_file = "manually_selected_clean.txt"
+embedding_sentences_file = "selected_and_positive_tweets.txt"
 
 corpus = get_all_sentences(embedding_sentences_file)
 
 corpus_embeddings = embedder.encode(corpus)
 
 save_embedding(embedding_sentences_file,corpus_embeddings)
+
+embedding_sentences_file = "texts_clean.txt"
+
+corpus = get_all_sentences(embedding_sentences_file)
+
+corpus_embeddings = embedder.encode(corpus)
+
+save_embedding(embedding_sentences_file,corpus_embeddings)
+
+'''
 # Perform kmean clustering
 num_clusters = 1
 clustering_model = KMeans(n_clusters=num_clusters)
@@ -40,7 +52,7 @@ cluster_assignment = clustering_model.labels_
 clustered_sentences = [[] for i in range(num_clusters)]
 for sentence_id, cluster_id in enumerate(cluster_assignment):
     clustered_sentences[cluster_id].append(corpus[sentence_id])
-'''
+
 for i, cluster in enumerate(clustered_sentences):
     print("Cluster ", i+1)
     print(cluster)
