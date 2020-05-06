f37f8f89809591e2de67069b2fe8aa5e6cce839a
qilei123 <qileimail123@gmail.com>
2020-05-05 22:58:31 -0400

    use bert

--- sentense_encode.py
+++ sentense_encode.py
@@ -1,30 +1,27 @@
-"""
-This is a simple application for sentence embeddings: clustering
-
-Sentences are mapped to sentence embeddings and then k-mean clustering is applied.
-"""
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
 
 embedder = SentenceTransformer('bert-base-nli-mean-tokens')
+embedder = SentenceTransformer('bert-large-nli-mean-tokens')
+embedder = SentenceTransformer('roberta-base-nli-mean-tokens')
+embedder = SentenceTransformer('roberta-large-nli-mean-tokens')
+embedder = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
 # Corpus with example sentences
-corpus = ['A man is eating food.',
-          'A man is eating a piece of bread.',
-          'A man is eating pasta.',
-          'The girl is carrying a baby.',
-          'The baby is carried by the woman',
-          'A man is riding a horse.',
-          'A man is riding a white horse on an enclosed ground.',
-          'A monkey is playing drums.',
-          'Someone in a gorilla costume is playing a set of drums.',
-          'A cheetah is running behind its prey.',
-          'A cheetah chases prey on across a field.'
-          ]
+def get_all_sentences(file_name):
+    file_header = open(file_name)
+
+    line = file_header.readline()
+    sentence_list = []
+    while line:
+        line.replace("\n","")
+        sentence_list.append(line)
+        line = file_header.readline()
+corpus = get_all_sentences("manually_selected_clean.txt")
 corpus_embeddings = embedder.encode(corpus)
 
 # Perform kmean clustering
-num_clusters = 5
+num_clusters = 1
 clustering_model = KMeans(n_clusters=num_clusters)
 clustering_model.fit(corpus_embeddings)
 cluster_assignment = clustering_model.labels_
