69c6e3abaf6d44261825f977ff73c3121bd3838e
qilei123 <qileimail123@gmail.com>
2020-05-06 01:10:18 -0400

    upload all

--- sentense_embedding.py
+++ sentense_embedding.py
@@ -13,9 +13,9 @@ def save_embedding(file_name,embedding_list):
         for code in embedded_codes:
             save_file_header.write(str(code)+" ")
         save_file_header.write("\n")
+
 def get_all_sentences(file_name):
     file_header = open(file_name)
-
     line = file_header.readline()
     sentence_list = []
     while line:
@@ -25,29 +25,37 @@ def get_all_sentences(file_name):
         sentence_list.append(line)
         line = file_header.readline()
     return sentence_list
-##################################
-embedding_sentences_file1 = "selected_and_positive_tweets.txt"
 
-corpus1 = get_all_sentences(embedding_sentences_file1)
+def load_embedded_codes(file_name):
+    file_header = open(file_name)
+    line = file_header.readline()
+    feature_list = []
+    while line:
+        line.replace("\n","")
+        eles = line.split(" ")
+        feature = []
+        for ele in eles:
+            feature.append(float(ele))
+        feature_list.append(feature)
+        line = file_header.readline()
+    return feature_list
 
-corpus_embeddings1 = embedder.encode(corpus1)
-'''
-save_embedding(embedding_sentences_file1,corpus_embeddings1)
-'''
-###################################
 
-embedding_sentences_file2 = "texts_clean.txt"
+def embed(embedder,src_file,will_save=False):
+    corpus = get_all_sentences(src_file)
 
-corpus2 = get_all_sentences(embedding_sentences_file2)
+    corpus_embeddings = embedder.encode(corpus)
+    if will_save:
+        save_embedding(src_file,corpus_embeddings)
+    return corpus_embeddings
 
-corpus_embeddings2 = embedder.encode(corpus2)
-'''
-save_embedding(embedding_sentences_file2,corpus_embeddings2)
-'''
+embeddings_positives = embed(embedder,"selected_and_positive_tweets.txt",False)
+
+embeddings_unknowns = embed(embedder,"texts_clean.txt",False)
 
 # Perform kmean clustering
 num_clusters = 1
 clustering_model = KMeans(n_clusters=num_clusters)
-clustering_model.fit(corpus_embeddings1)
+clustering_model.fit(embeddings_positives)
 print("cluster_center is:")
 print(clustering_model.cluster_centers_)
